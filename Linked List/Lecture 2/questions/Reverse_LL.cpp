d0f4cff0cd31dee78c35149cf96ff085f54834ad
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2023-04-22 12:39:51 +0530

    reverse LL

--- Linked List/Lecture 2/questions/Reverse_LL.cpp
+++ Linked List/Lecture 2/questions/Reverse_LL.cpp
@@ -1,5 +1,88 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int data)
+{
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *reverseUsingLoop(Node *&prev, Node *&curr)
+{
+
+    while (curr != NULL)
+    {
+        Node *temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
+}
+
+Node *reverse(Node *&prev, Node *&curr)
+{
+    // bc
+    if (curr == NULL)
+    {
+        // LL is reverse
+        return prev;
+    }
+
+    // normal case
+    // 1 case solve then recursion will take care
+    Node *forward = curr -> next;
+    curr->next = prev;
+
+    reverse(curr, forward);
+}
+
+int main()
+{
+    Node *head = new Node(40);
+
+    insertAtHead(head, 30);
+    insertAtHead(head, 20);
+    insertAtHead(head, 10);
+
+    print(head);
+    cout << endl;
+
+    Node *prev = NULL;
+    Node *curr = head;
+    // head = reverse(prev, curr);
+    cout << endl;
+    cout<<"Printing Reverse Linked List"<<endl;
+    head = reverseUsingLoop(prev, curr);
+    print(head);
 }
