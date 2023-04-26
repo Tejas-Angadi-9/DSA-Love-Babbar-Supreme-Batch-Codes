cdb8923e9c2e6f6a0c36ac03dbdce5bf6e5e09ed
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2023-04-25 20:18:04 -0700

    stack

---
+++ Stack_/Lecture 1/intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    // creating of stack
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+
+    // pop
+    st.pop();
+
+    // checking top element
+    cout << "Element on top is: " << st.top() << endl;
+
+    // size
+    cout << "Size of stack is: " << st.size() << endl;
+
+    if (st.empty())
+        cout << "Stack is empty" << endl;
+    else
+        cout << "Stack is not empty" << endl;
+}
